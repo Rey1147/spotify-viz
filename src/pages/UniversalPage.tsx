e890d6921ece3649cbbbd34fad071cba9f08cd88
Evgenia <118038480+Rey1147@users.noreply.github.com>
2025-10-06 01:09:03 +0300

    add Montserrat font and styles to universal page

--- src/pages/UniversalPage.tsx
+++ src/pages/UniversalPage.tsx
@@ -2,17 +2,21 @@
 import Player from "@/components/Player"
 import useCurrentlyPlaying from "@/hooks/useCurrentlyPlaying"
 
+import styles from "../styles/index.module.scss"
+
 const UniversalPage = ({ children }: { children: React.ReactNode }) => {
   const { nowPlaying } = useCurrentlyPlaying()
 
   return (
-    <div>
-      {children}
-      <Player
-        track={nowPlaying?.item ?? null}
-        isPlaying={nowPlaying?.is_playing}
-        progressMs={nowPlaying?.progress_ms ?? null}
-      />
+    <div className={styles.page}>
+      <main className={styles.page__main}>
+        {children}
+        <Player
+          track={nowPlaying?.item ?? null}
+          isPlaying={nowPlaying?.is_playing}
+          progressMs={nowPlaying?.progress_ms ?? null}
+        />
+      </main>
     </div>
   )
 }
