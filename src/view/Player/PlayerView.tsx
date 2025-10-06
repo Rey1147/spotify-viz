d9fa46c753a260e1665ab62de4db94a32d37cbc3
Evgenia <118038480+Rey1147@users.noreply.github.com>
2025-10-06 18:06:20 +0300

    fix player style

--- src/view/Player/PlayerView.tsx
+++ src/view/Player/PlayerView.tsx
@@ -8,7 +8,7 @@ type PlayerViewProps = {
     album: { name: string }
   } | null | undefined
   artists: string | undefined
-  isPlaying: boolean | undefined
+  isPlaying?: boolean | undefined
   formatDuration: (ms: number) => string
   displayProgress: number,
   durationMs: number
@@ -17,14 +17,19 @@ type PlayerViewProps = {
 // TODO: вынести прогресс бар в отдельный компонент и добавить логику при product === 
 
 const PlayerView = (props: PlayerViewProps) => {
-  const {albumImage, track, artists, isPlaying, formatDuration, displayProgress, durationMs} = props
+  const {albumImage, track, artists, formatDuration, displayProgress, durationMs} = props
   return (
     <div className={styles.player}>
       {albumImage && (
         <img src={albumImage.url} className={styles.player__image} alt={track?.album.name}/>
       )}
       <div className={styles.player__track}>
-        <div className={styles.player__track_name}>{track?.name}{track?.explicit && <span> E</span>}</div>
+        <div className={styles.player__track_name}>
+          <span className={styles.player__track_name_inline}>
+            {track?.name}
+            {track?.explicit && <span>E</span>}
+          </span>
+        </div>
         <div className={styles.player__track_artists}>{artists}</div>
       </div>
       <div className={styles.player__controls}>
