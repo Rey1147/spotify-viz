0ca6615b238927e94976ba1a5464cac2cf0680ec
Evgenia <118038480+Rey1147@users.noreply.github.com>
2025-10-06 18:00:42 +0300

    fix logout function

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -1,9 +1,15 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, Navigate } from "react-router-dom"
 import { ErrorBoundary } from "react-error-boundary"
 import { Suspense } from "react"
 import UniversalPage from "@/pages/UniversalPage"
+import useAuth from "@/hooks/useAuth"
 
 const Protected = () => {
+  const { status } = useAuth()
+
+  if (status === "unauthenticated") {
+    return <Navigate to="/login" replace />
+  }
 
   return (
     <ErrorBoundary fallbackRender={() => <div>Не удалось загрузить пользователя</div>}>
